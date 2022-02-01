3e0a1f2aa30dee8a632a683b6455fe0f950166ed
susanaabraham <YOUR@EMAIL.com>
2022-02-01 17:05:45 +0100

    created a botton/not active discussion comment

--- src/pages/MyBooks/index.js
+++ src/pages/MyBooks/index.js
@@ -5,8 +5,8 @@ import Container from "react-bootstrap/Container";
 import Book from "../../components/Book";
 import { selectMyBooks } from "../../store/books/selectors";
 import { fetchMyBooks } from "../../store/books/actions";
-
-import "./myBooks.css";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 export default function MyBooks() {
   const dispatch = useDispatch();
@@ -16,18 +16,36 @@ export default function MyBooks() {
     dispatch(fetchMyBooks());
   }, [dispatch]);
 
-  return (
-    <>
-      <Jumbotron>
-        <h1>My Discussions</h1>
-      </Jumbotron>
-      <div className="myBookList">
-        <Container>
-          {myBooks.map((book) => {
-            return <Book key={book.id} id={book.id} title={book.title} lccn={book.lccn} coverUrl={book.coverUrl}/>;
-          })}
-        </Container>
-      </div>
-    </>
-  );
+return (
+  <>
+  <Jumbotron>
+    <h1>My Discussions</h1>
+  </Jumbotron>
+  {myBooks.length !== 0 ? (
+  <Container>
+    {myBooks.map(book => {
+      return(
+        <Book
+        key={book.id}
+        id={book.id}
+        title={book.title}
+        />
+      );
+    })}
+  </Container>
+  ) : 
+  <Container>
+    <p>You don't have any active discussions yet.</p>  
+    <Link to={`/`}>
+      <Button>Discover</Button>
+    </Link>
+    <br/>
+    <br/>
+    <Link to={`/search`}>
+      <Button>Add a new discussion</Button>
+    </Link>
+  </Container>
+  }
+  </>
+);
 }
