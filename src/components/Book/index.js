20e6870d31af7d1d0c96a21ecb1ddef779dd6434
andre-mota <andremota3@gmail.com>
2022-02-01 15:05:05 +0100

    Link to discussion page on button click.

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./book.css";
 
 export default function Book(props) {
+  const discussionPage = `/discussions/${props.id}`;
   return (
     <div className="bookCard">
-    <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src={props.coverUrl} />
-    <Card.Body>
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>{props.lccn}</Card.Text>
-      <Button variant="primary">Discuss</Button>
-    </Card.Body>
-  </Card>
-  </div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Img variant="top" src={props.coverUrl} />
+          <Card.Title style={{ marginTop: "5%", marginBottom: "5%" }}>
+            {props.title}
+          </Card.Title>
+          <Link to={discussionPage}>
+            <Button variant="primary">Go to discussion</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
